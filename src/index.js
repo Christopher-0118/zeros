91c8170ab9ee9633b64bfe2dc1de9bd5a1b9bc49
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-02 17:30:28 +0300

    zeros:feature/ implement getZerosCount method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let maxDegree = Math.floor(Math.log(number) / Math.log(5));
+  let currentNum = 5;
+  let zerosCount = 0;
+
+  for( let currentDegree = 1; currentDegree <= maxDegree; currentDegree++) {
+    zerosCount +=  Math.floor(number / currentNum);
+    currentNum = currentNum * 5;
+  }
+  return zerosCount;
 }
